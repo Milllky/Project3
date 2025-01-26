fe22e74b08d72251fca788d0416aeee96b22af9b
Trofimov M <milllky@rocketmail.com>
2025-01-26 16:40:25 +0500

    Add Math.pow

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double weightKg, double heightM) {
-        int result = (int) (weightKg / (heightM * heightM));
+        int result = (int) (weightKg / Math.pow(heightM, 2));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int index = (int) service.calculate(79.5, 1.76);
+        int index = (int) service.calculate(98, 1.87);
         System.out.println(index);
     }
 }
